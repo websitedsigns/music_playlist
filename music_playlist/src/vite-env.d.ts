888b416f6e31a42e8de4316c016b37a4c4a6b4cd
websitedsigns <websitedsigns@gmail.com>
2023-09-19 18:49:06 +0100

    add current track

--- music_playlist/src/vite-env.d.ts
+++ music_playlist/src/vite-env.d.ts
@@ -1,12 +1,14 @@
 /// <reference types="vite/client" />
+
 type Track = {
     id: number,
     title: string,
-    artist: string,
+    artist: string,  
     album: string,
     length: string,
   };
 
   type PlaylistItemProps = {
     track: Track,
-  }
+    state: 'playing' | 'paused',
+  };

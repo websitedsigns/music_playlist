8d9d6d6ee9c8429b90a534c2bd103bfc56478b6b
websitedsigns <websitedsigns@gmail.com>
2023-09-19 18:38:39 +0100

    revert to previous code
    created error

--- music_playlist/src/PlaylistItem.tsx
+++ music_playlist/src/PlaylistItem.tsx
@@ -1,7 +1,7 @@
 import Icon from '@mdi/react';
 import { mdiMusic, mdiPause, mdiPlay } from '@mdi/js';
 
-function PlaylistItem({ track }: PlaylistItemProps) {
+function PlaylistItem() {
   return (
     <li className="flex flex-row px-3 py-2 space-x-4 mb-2">
       <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
@@ -9,13 +9,13 @@ function PlaylistItem({ track }: PlaylistItemProps) {
       </div>
       <div className="flex-1">
         <div className="flex flex-row">
-          <h2 className="flex-1 font-bold">{track.title}</h2>
-          <p className="text-neutral-200">{track.length}</p>
+          <h2 className="flex-1 font-bold">Track title</h2>
+          <p className="text-neutral-200">04:23</p>
         </div>
-        <p>{track.artist} - {track.album}</p>
+        <p>Artist Name - Album Name</p>
       </div>
       <button className="w-12 h-12 p-2" aria-label="Pause playing this track">
-        <Icon path={mdiPlay} />
+        <Icon path={mdiPause} />
       </button>
     </li>
   )

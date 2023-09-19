7a3a9b6fb0e4a5839b594e3bedcb0e0bcf9673e1
websitedsigns <websitedsigns@gmail.com>
2023-09-19 18:24:55 +0100

    static front end

--- music_playlist/src/App.tsx
+++ music_playlist/src/App.tsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Icon from '@mdi/react';
+import { mdiMusic, mdiPause, mdiPlay } from '@mdi/js';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-slate-800 text-white">
+      <main className="max-w-screen-md mx-auto">
+        <header className="bg-pink-600 text-2xl font-bold p-3">
+          <h1>Sounds Good! - Epic Playlist</h1>
+        </header>
+        <ol className="border-4 border-pink-600">
+          <li className="flex flex-row px-3 py-2 space-x-4 mb-2">
+            <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
+              <Icon path={mdiMusic} />
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-row">
+                <h2 className="flex-1 font-bold">Track title</h2>
+                <p className="text-neutral-200">04:23</p>
+              </div>
+              <p>Artist Name - Album Name</p>
+            </div>
+            <button className="w-12 h-12 p-2" aria-label="Pause playing this track">
+              <Icon path={mdiPause} />
+            </button>
+          </li>
+          <li className="flex flex-row px-3 py-2 space-x-4 mb-2">
+            <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
+              <Icon path={mdiMusic} />
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-row">
+                <h2 className="flex-1">Track title</h2>
+                <p className="text-neutral-200">04:23</p>
+              </div>
+              <p>Artist Name - Album Name</p>
+            </div>
+            <button className="w-12 h-12 p-2" aria-label="Play this track">
+              <Icon path={mdiPlay} />
+            </button>
+          </li>
+          <li className="flex flex-row px-3 py-2 space-x-4">
+            <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
+              <Icon path={mdiMusic} />
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-row">
+                <h2 className="flex-1">Track title</h2>
+                <p className="text-neutral-200">04:23</p>
+              </div>
+              <p>Artist Name - Album Name</p>
+            </div>
+            <button className="w-12 h-12 p-2" aria-label="Play this track">
+              <Icon path={mdiPlay} />
+            </button>
+          </li>
+        </ol>
+      </main>
+    </div>
   )
 }
 

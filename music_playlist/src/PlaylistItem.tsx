0930699eeb2406cb86c415d5d095f72ff840e8b2
websitedsigns <websitedsigns@gmail.com>
2023-09-19 18:42:35 +0100

    update app
    to pass component to child

--- music_playlist/src/PlaylistItem.tsx
+++ music_playlist/src/PlaylistItem.tsx
@@ -1,24 +1,47 @@
 import Icon from '@mdi/react';
+
 import { mdiMusic, mdiPause, mdiPlay } from '@mdi/js';
 
-function PlaylistItem() {
+
+
+function PlaylistItem({ track }: PlaylistItemProps) {
+
   return (
+
     <li className="flex flex-row px-3 py-2 space-x-4 mb-2">
+
       <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
+
         <Icon path={mdiMusic} />
+
       </div>
+
       <div className="flex-1">
+
         <div className="flex flex-row">
-          <h2 className="flex-1 font-bold">Track title</h2>
-          <p className="text-neutral-200">04:23</p>
+
+          <h2 className="flex-1 font-bold">{track.title}</h2>
+
+          <p className="text-neutral-200">{track.length}</p>
+
         </div>
-        <p>Artist Name - Album Name</p>
+
+        <p>{track.artist} - {track.album}</p>
+
       </div>
+
       <button className="w-12 h-12 p-2" aria-label="Pause playing this track">
-        <Icon path={mdiPause} />
+
+        <Icon path={mdiPlay} />
+
       </button>
+
     </li>
+
   )
+
 }
+
+
 
 export default PlaylistItem

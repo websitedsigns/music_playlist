75ad1f0cadd943014f30f3ac02797460da9007cf
websitedsigns <websitedsigns@gmail.com>
2023-09-19 18:45:16 +0100

    neaten code

--- music_playlist/src/App.tsx
+++ music_playlist/src/App.tsx
@@ -1,85 +1,47 @@
 import { useState } from 'react';
-
 import PlaylistItem from './PlaylistItem';
 
-
-
 function App() {
-
   const [ items ] = useState([
-
     {
-
       id: 1,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
 
     {
-
       id: 2,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
 
     {
-
       id: 3,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
-
   ] as Track[]);
-
-
 
   return (
 
     <div className="min-h-screen bg-slate-800 text-white">
-
       <main className="max-w-screen-md mx-auto">
-
         <header className="bg-pink-600 text-2xl font-bold p-3">
-
           <h1>Sounds Good! - Epic Playlist</h1>
-
         </header>
 
         <ol className="border-4 border-pink-600">
-
           {items.map((track) => { return <PlaylistItem key={track.id} track={track}/>})}
-
         </ol>
-
       </main>
-
     </div>
-
   )
-
 }
-
-
 
 export default App
--- music_playlist/src/PlaylistItem.tsx
+++ music_playlist/src/PlaylistItem.tsx
@@ -1,47 +1,24 @@
 import Icon from '@mdi/react';
-
 import { mdiMusic, mdiPause, mdiPlay } from '@mdi/js';
 
-
-
 function PlaylistItem({ track }: PlaylistItemProps) {
-
   return (
-
     <li className="flex flex-row px-3 py-2 space-x-4 mb-2">
-
       <div className="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
-
         <Icon path={mdiMusic} />
-
       </div>
-
       <div className="flex-1">
-
         <div className="flex flex-row">
-
           <h2 className="flex-1 font-bold">{track.title}</h2>
-
           <p className="text-neutral-200">{track.length}</p>
-
         </div>
-
         <p>{track.artist} - {track.album}</p>
-
       </div>
-
       <button className="w-12 h-12 p-2" aria-label="Pause playing this track">
-
         <Icon path={mdiPlay} />
-
       </button>
-
     </li>
-
   )
-
 }
-
-
 
 export default PlaylistItem
